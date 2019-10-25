a2d1fddf6867babd2f0ad7b3a230c011376927b2
makoto <mack@mackstudio.net>
2019-10-25 09:33:11 -0400

    now uploads last 2 weeks of data at 2am

--- zexperiments/start_flash_report.rb
+++ zexperiments/start_flash_report.rb
@@ -17,9 +17,20 @@ loop do
       puts "."
     else
       print "hibernating until 11am"
+      loaded_past_history = false
       while Time.now.hour <= 10 || Time.now.hour >= 22 do
         (60 * 60).times do
           sleep 1; Thread.pass
+
+          if Time.now.hour == 1 && !loaded_past_history
+            14.times do | days_ago |
+              puts days_ago
+              FlashReport.new.run( days_ago )
+              sleep 1
+            end
+
+            loaded_past_history = true
+          end
         end
       end
     end
